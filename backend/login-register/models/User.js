4f677d1e08aab6d717ef1c28ddf03f24733f859c
shivika24 <43493690+shivika24@users.noreply.github.com>
2020-01-25 11:31:31 +0530

    backend

---
+++ backend/login-register/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create Schema
+const UserSchema = new Schema({
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  pno: {
+    type:String,
+    required:true
+  },
+  tourist: {
+    type:String,
+    required:true
+  },
+  role : {
+    type:String,
+    required:true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  mainid: {
+    type: String,
+  }
+})
+
+module.exports = User = mongoose.model('users', UserSchema)
---
+++ backend/readme.txt
@@ -0,0 +1,4 @@
+JSON server is running on  port 4000
+login-register is running on port 5000
+database is running on port 8081
+react js(fronend) is running on port 3000
